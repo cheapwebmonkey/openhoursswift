141176c220c2b52e5a352ac510cc3ba384e56ccf
Margeaux Spring <contact@cheapwebmonkey.com>
2016-03-21 17:03:56 -0400

    "added Fabric. It is awesome"

--- openhoursapp/AppDelegate.swift
+++ openhoursapp/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Fabric
+import TwitterKit
+import Answers
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Answers.self, Twitter.self])
+
+
         return true
+        
     }
 
     func applicationWillResignActive(application: UIApplication) {
